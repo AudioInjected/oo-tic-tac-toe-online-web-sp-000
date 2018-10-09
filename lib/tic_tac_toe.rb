775763f2d5f8cda487b11fc62d55af2924a42eda
Rafael Batista <rafaelb24@yahoo.com>
2018-10-09 17:42:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0, 8) && !position_taken?(@board, index)
+    if @index.between?(0, 8) && !position_taken?(@board, @index)
       true
     else
       false
